a48665e75a0bb109395c07f922c2bf981ac14ce8
lfgg123 <30794147+lfgg123@users.noreply.github.com>
2019-02-14 15:12:37 +0800

    Update job_config.sql

--- sql/job_config.sql
+++ sql/job_config.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `scheduler_job` (
   `id` bigint(10) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `job_name` varchar(64) NOT NULL COMMENT '作业名称',
-  `dubbo_class` varchar(256) NOT NULL COMMENT 'dubbo接口class全路径',
+  `method_class` varchar(256) NOT NULL COMMENT 'class全路径',
   `method_name` varchar(256) NOT NULL COMMENT '方法名',
   `sharding_total_count` smallint(5) DEFAULT '2' COMMENT '分片总数',
   `cron` varchar(64) NOT NULL COMMENT '作业触发时间表达式',
